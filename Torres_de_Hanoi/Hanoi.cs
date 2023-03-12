ea958c8a2564683195acd3108f0368a11e88292f
JorgeHelados <125460859+JorgeHelados@users.noreply.github.com>
2023-03-12 20:14:40 +0100

    Implementacion clase Hanoi

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -9,15 +9,39 @@ namespace Torres_de_Hanoi
     class Hanoi
     {
         /*TODO: Implementar métodos*/
-        public void mover_disco(Pila a, Pila b)
-        {
-
+        public void mover_disco(Pila a, Pila b){
+            if(a.Top < b.Top){
+                b.push(a.Pop());
+            }
+            else{
+                a.push(b.Pop());
+            }
         }
 
-        public int iterativo(int n, Pila ini, Pila fin, Pila aux)
-        {
-            return 0;
+        public int iterativo(int n, Pila ini, Pila fin, Pila aux) {
+            int m = 0;
+            if (n % 2 == 0) {
+                while (fin.Size != 3) {
+                    mover_disco(ini, fin);
+                    m = m + 1;
+                    mover_disco(ini, aux);
+                    m = m + 1;
+                    mover_disco(aux, fin);
+                    m = m + 1;
+                }
+                return m;
+            }
+            else {
+                while (fin.Size != 3) {
+                    mover_disco(ini, aux);
+                    m = m + 1;
+                    mover_disco(ini, fin);
+                    m = m + 1;
+                    mover_disco(aux, fin);
+                    m = m + 1;
+                }
+                return m;
+            }
         }
-
     }
 }
